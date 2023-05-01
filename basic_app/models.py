ea369115f5a7f057037b1b182ce6dc60bd487622
ulugbekerkinovich2 <ulugbek9015y@gmail.com>
2023-05-01 10:00:16 +0500

    'update_models'

--- basic_app/models.py
+++ basic_app/models.py
@@ -154,7 +154,7 @@ class Category(models.Model):
     name_en = models.CharField(max_length=255, default='none', null=True, blank=True)
 
     def __str__(self):
-        return f"{self.name_ru}-{self.name_en}-{self.name_uz}"
+        return self.name_ru
 
 
 # class User(models.Model):
